de64a13973b91c6bffc12a2db371a760f0290def
Ujjwal Gautam <ujjwal97@hotmail.com>
2025-08-09 20:47:55 +0530

    add GHA workflows

--- teleheal-backend/src/main/java/com/fomo/teleheal/TeleHealApplication.java
+++ teleheal-backend/src/main/java/com/fomo/teleheal/TeleHealApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TelehealApplication {
+public class TeleHealApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TelehealApplication.class, args);
+		SpringApplication.run(TeleHealApplication.class, args);
 	}
 
 }
