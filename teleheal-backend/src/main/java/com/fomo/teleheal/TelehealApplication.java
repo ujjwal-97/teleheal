059ff8553cdade60cda2f7b914a3c9fed90599ac
Ujjwal Gautam <ujjwal97@hotmail.com>
2025-08-09 19:58:50 +0530

    init backend

---
+++ teleheal-backend/src/main/java/com/fomo/teleheal/TelehealApplication.java
@@ -0,0 +1,13 @@
+package com.fomo.teleheal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelehealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelehealApplication.class, args);
+	}
+
+}
---
+++ teleheal-backend/src/test/java/com/fomo/teleheal/TelehealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fomo.teleheal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelehealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
